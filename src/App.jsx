710f951cdd455286cadece591b35ffaa626068c2
paavansaireddy <79843129+paavansaireddy@users.noreply.github.com>
2025-01-18 17:38:57 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
 import Intro from './components/intro/Intro';
 import Portfolio from './components/portfolio/Portfolio';
-import Testimonials from './components/testimonials/Testimonials';
 import Topbar from './components/topbar/Topbar';
 import Exp from './components/exp/exp';
 
